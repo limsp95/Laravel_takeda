09a837c86825742c7c2f40d95527e2c7f788bd48
Unknown <www.limsp95@gmail.com>
2021-02-27 00:35:16 +0800

    First init

---
+++ app/Admin/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->get('/example', 'ExampleController@form')->name('example');
+    $router->resource('demo/users', UserController::class);
+
+});
